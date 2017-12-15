099fe3da3b4f7698b89b0d6a53d313ecc3988d0a
Unknown <stobenetti@gmail.com>
2017-12-15 16:58:41 -0200

    View comum: footer

---
+++ views/common/footer.php
@@ -0,0 +1,10 @@
+    <!-- JQuery -->
+    <script type="text/javascript" src="<?= base_url('assets/mdb/js/jquery-3.1.1.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/mdb/js/tether.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/mdb/js/bootstrap.min.js') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('assets/mdb/js/mdb.min.js') ?>"></script>
+    <?php echo isset($extra_script) ? $extra_script : '' ?>
+
+</body>
+
+</html>
