a8fcc0f1443988ceb2d4976f6833be488bc17d79
Unknown <stobenetti@gmail.com>
2017-12-21 14:30:47 -0200

    Atualização para MDB4

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,10 +1,12 @@
-    <!-- JQuery -->
-    <script type="text/javascript" src="<?= base_url('assets/mdb/js/jquery-3.1.1.min.js') ?>"></script>
-    <script type="text/javascript" src="<?= base_url('assets/mdb/js/tether.min.js') ?>"></script>
-    <script type="text/javascript" src="<?= base_url('assets/mdb/js/bootstrap.min.js') ?>"></script>
-    <script type="text/javascript" src="<?= base_url('assets/mdb/js/mdb.min.js') ?>"></script>
-    <?php echo isset($extra_script) ? $extra_script : '' ?>
-
+<!--        <script type="text/javascript" src="--><? //= base_url('assets/js/jquery.js') ?><!--"></script>-->
+<!-- JQuery -->
+<script type="text/javascript" src="<?= base_url('assets/mdb/js/jquery-3.2.1.min.js') ?>"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="<?= base_url('assets/mdb/js/popper.min.js') ?>"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="<?= base_url('assets/mdb/js/bootstrap.min.js') ?>"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="<?= base_url('assets/mdb/js/mdb.min.js') ?>"></script>
+<?= $extra_script ?>
 </body>
-
 </html>
--- views/common/header.php
+++ views/common/header.php
@@ -3,14 +3,21 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Dentalbit</title>
 
+    <title>LPS-CI</title>
+
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
     <link href="<?= base_url('assets/mdb/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
     <link href="<?= base_url('assets/mdb/css/mdb.min.css') ?>" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="<?= base_url('assets/mdb/css/style.css') ?>" rel="stylesheet">
     <?= $extra_style ?>
+
 </head>
 
 <body>
